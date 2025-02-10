92652da480f7e74653908c8ea70871f51bb8807b
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2025-02-10 23:52:19 +0530

    Project setuped

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Chat app</h1>
+    </>
+  )
+}
+
+export default App
