6442b50fdfd18628f99b8b137a59d8487110de8a
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2025-03-06 01:00:27 +0530

    New: validation created

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -19,16 +19,16 @@ function Signup() {
   };
   const formValidation = () => {
     setErrors({
-      username: formData.username == "" ? "EnterValid username" : null,
+      username: formData.username == "" ? "* Enter Valid username" : null,
       email: !/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/.test(
         formData.email
       )
-        ? "Invalid email"
+        ? "* please enter a valid email"
         : null,
       password:
-        !formData.password >= 6 && /[a-fA-F0-9]/.test(formData.password)
-          ? "Please enter a valid password"
-          : null,
+        formData.password.length >= 6
+          ? null
+          : "* Please enter a valid password",
     });
 
     return Object.values(errors).every((error) => error === null);
@@ -36,15 +36,21 @@ function Signup() {
   const handleSubmit = (e) => {
     e.preventDefault();
     // Validate form data
-    console.log(formValidation());
-
-    // TODO: Implement login logic here
-    console.log(formData);
+    if (formValidation()) {
+      // Reset form data
+      setFormData({
+        username: "",
+        email: "",
+        password: "",
+      });
+      // TODO: Implement signup logic here
+      console.log(formData);
+    }
   };
   return (
     <div className="grid gap-4 md:grid-cols-2 bg-zinc-900 items-center h-screen">
       {/* LoginForm */}
-      <div className="max-w-full">
+      <div className="max-w-full p-5">
         <div className="flex justify-center p-10">
           <img src="./chat.png" className="w-20 "></img>
         </div>
@@ -62,23 +68,27 @@ function Signup() {
             name="username"
             placeholder="Username"
           />
+          {errors.username && <p className="text-red-800">{errors.username}</p>}
           <label className="block text-white mt-3">email :</label>
           <input
             value={formData.email}
             onChange={handleChange}
             className="mt-3 text-white placeholder:text-white p-4 w-full border-2 rounded-md h-14 border-gray-300 focus:outline-none focus:border-amber-600"
-            type="email"
+            type="text"
             name="email"
             placeholder="email"
           />
+          {errors.email && <p className="text-red-800">{errors.email}</p>}
           <label className="block mt-3 text-white">Password :</label>
           <input
             value={formData.password}
             onChange={handleChange}
             className="mt-3 p-4 w-full border-2 rounded-md h-14 text-white placeholder:text-white border-gray-300 focus:outline-none focus:border-amber-600"
             type="password"
+            name="password"
             placeholder="Password"
           />
+          {errors.password && <p className="text-red-800">{errors.password}</p>}
           <button className="w-full h-14 p-2 mt-3 rounded-md bg-blue-500 text-white hover:bg-blue-600">
             Register
           </button>
