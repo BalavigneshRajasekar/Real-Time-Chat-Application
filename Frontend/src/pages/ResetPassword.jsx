dfb2878d482fd3a8a6619f91c7e83511f0e5dcfa
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2025-03-21 22:41:38 +0530

    New: Auth system completed

---
+++ Frontend/src/pages/ResetPassword.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function ResetPassword() {
+  return (
+    <div>
+      <h1>Reset Password</h1>
+    </div>
+  );
+}
+
+export default ResetPassword;
