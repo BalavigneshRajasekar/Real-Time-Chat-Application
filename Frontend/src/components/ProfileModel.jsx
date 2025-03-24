d2ad58ee9e8bff15fb32c29f43b2e644ff30c3dd
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2025-03-24 23:00:46 +0530

    New : profile model implemented

---
+++ Frontend/src/components/ProfileModel.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function ProfileModel() {
+  return (
+    <div>
+      <h1>Profile</h1>
+    </div>
+  );
+}
+
+export default ProfileModel;
