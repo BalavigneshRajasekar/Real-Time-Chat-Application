304ddbc5363f62f2fd7c2b269ad6ad2c299bd1c2
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2025-03-25 23:46:14 +0530

    New : Profile model created

--- Frontend/src/components/ProfileModel.jsx
+++ Frontend/src/components/ProfileModel.jsx
@@ -1,12 +1,9 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
 
-function ProfileModel() {
-  return (
-    <div>
-      <h1>Profile</h1>
-    </div>
-  );
+function ProfileModel({ show }) {
+  return <>{show && <h1>Profile</h1>}</>;
 }
 
 export default ProfileModel;
