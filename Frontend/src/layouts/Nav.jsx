9b20053c328e70cb19f3df68f1737e2b08ef7c95
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2025-03-27 23:17:26 +0530

    Fix : profile bug fixed

--- Frontend/src/layouts/Nav.jsx
+++ Frontend/src/layouts/Nav.jsx
@@ -4,7 +4,7 @@ import useAuth from "../hooks/useAuth";
 import { IoIosLogOut } from "react-icons/io";
 import { MdOutlineSettings } from "react-icons/md";
 import { useDispatch } from "react-redux";
-import { setLogoutModal } from "../store/asyncCalls";
+import { setLogoutModal, setProfileModal } from "../store/asyncCalls";
 
 function Nav() {
   const dispatch = useDispatch();
@@ -16,7 +16,10 @@ function Nav() {
           <h2>ChatHub</h2>
         </div>
         <div className="flex gap-3  p-5">
-          <p className="inline-block cursor-pointer px-3 py-1 w-fit hover:bg-white rounded-4xl  transition-all ease-in-out hover:scale-110 active:scale-90">
+          <p
+            onClick={() => dispatch(setProfileModal())}
+            className="inline-block cursor-pointer px-3 py-1 w-fit hover:bg-white rounded-4xl  transition-all ease-in-out hover:scale-110 active:scale-90"
+          >
             <MdOutlineSettings size={"35px"} />
           </p>
           <p
