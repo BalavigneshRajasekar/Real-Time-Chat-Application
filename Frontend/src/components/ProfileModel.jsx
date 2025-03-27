464db77a693fc9d390a8145250a860c23de4d99f
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2025-03-27 23:51:42 +0530

    New : Enhanced UI for Profile page

--- Frontend/src/components/ProfileModel.jsx
+++ Frontend/src/components/ProfileModel.jsx
@@ -2,22 +2,68 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { motion } from "motion/react";
-import { Avatar } from "antd";
+import { Avatar, Image, Upload } from "antd";
+import useAuth from "../hooks/useAuth";
+import { FaCamera } from "react-icons/fa";
 function ProfileModel() {
+  const { user } = useAuth();
   return (
     <>
+      {/* Animation div */}
       <motion.div
-        className="w-screen min-h-40 absolute rounded-2xl text-center top-52 flex justify-center"
+        className="w-screen absolute rounded-2xl text-center top-1 flex justify-center"
         initial={{ scale: 0 }}
         animate={{ scale: 1 }}
         exit={{ scale: 0 }}
         transition={{ type: "tween" }}
         key="logout-modal"
       >
-        <div className="w-fit h-full rounded-2xl p-10 bg-gray-900">
+        {/* Main Div */}
+        <div className="w-full lg:w-3xl h-full rounded-2xl p-10 bg-gray-900">
           <h2 className="text-white">Profile</h2>
+          <div className="w-full  h-fit">
+            <Image
+              src={user.profilePic ? user.profilePic : "user.png"}
+              style={{ width: "150px", height: "150px", borderRadius: "50%" }}
+            ></Image>
+            <Upload className="relative">
+              <FaCamera className="text-white text-2xl inline-block absolute bottom-0 hover:cursor-pointer active:scale-75 transition-all" />
+            </Upload>
+          </div>
+          {/* User Data Container */}
+          <div className="text-left">
+            <label className="text-white block" htmlFor="name">
+              Name:
+            </label>
+            <input
+              name="name"
+              value={user.username}
+              disabled
+              className="w-full text-white uppercase  p-3 rounded-xl bg-gradient-to-l   from-orange-300 to-orange-900"
+            ></input>
+            <label className="text-white mt-3 inline-block " htmlFor="email">
+              Email:
+            </label>
+            <input
+              name="email"
+              value={user.email}
+              disabled
+              className="w-full text-white uppercase  p-3 rounded-xl bg-gradient-to-l   from-orange-300 to-orange-900"
+            ></input>
+          </div>
+          {/* Group List */}
           <div>
-            <Avatar src={"user.png"} className="w-50 "></Avatar>
+            <h2 className="text-white mt-10 text-left">Groups IN</h2>
+            <div className="flex text-white">
+              {/* Group Card */}
+              <div>
+                <Avatar
+                  src={"chat.png"}
+                  style={{ width: "50px", height: "50px" }}
+                ></Avatar>
+                <h3>Friends Forever</h3>
+              </div>
+            </div>
           </div>
         </div>
       </motion.div>
