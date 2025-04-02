5399233ab763d523855ba5d6f9c451498fc303fe
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2025-04-02 20:21:33 +0530

    New : profile api flow changed

--- Backend/controller/userFlow.controller.js
+++ Backend/controller/userFlow.controller.js
@@ -3,19 +3,13 @@ const userService = require("../Services/user.service");
 
 class AppFLow {
   async addProfile(req, res) {
-    const { displayName } = req.body;
     try {
-      if (!displayName) {
-        return res.status(400).json({ message: "Display name is required" });
-      }
       // Get the user from the request
       const user = req.user;
       // Upload profile image to cloudinary
       const imageUrl = await Utilities.uploadImage(req);
       // Update this imageURL and Display name in DB
-
       await userService.findAndUpdateUser(user.email, {
-        displayName: displayName,
         profilePic: imageUrl,
       });
       return res.status(200).json({ message: "Profile updated successfully" });
