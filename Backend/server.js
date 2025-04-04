df0ffae3d32e76bc6de74f1c195d48753499f351
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2025-04-04 20:14:06 +0530

    Fix : New Origin added

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,9 @@ const httpForSocket = http.createServer(server);
 server.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173", "https://chathub-three-flax.vercel.app"],
+    origin: "https://chathub-three-flax.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     // Allow all origins to connect to the server
   })
 ); //Handle Cors for entire app
@@ -32,7 +34,7 @@ server.use(express.urlencoded({ extended: true })); // accept nested object and 
 // Socket initialized with server
 const io = socketIo(httpForSocket, {
   cors: {
-    origin: ["http://localhost:5173", "https://chathub-three-flax.vercel.app"],
+    origin: "https://chathub-three-flax.vercel.app",
   },
 });
 // Socket connection entry point
