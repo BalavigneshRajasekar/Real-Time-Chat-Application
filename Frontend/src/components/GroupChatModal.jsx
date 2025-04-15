573d000acd63dda13299752d547519d7f91d4156
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2025-04-15 20:57:56 +0530

    New : group chat model created

--- Frontend/src/components/GroupChatModal.jsx
+++ Frontend/src/components/GroupChatModal.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
 
-function GroupChatModal() {
+function GroupChatModal({ open }) {
   return (
     <div>
       <h1>Create group Chat Modal</h1>
