d24d895e18307bf71807f6730df41008df183b30
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2025-04-17 21:20:12 +0530

    New : enhanced new group chat modal

--- Frontend/src/components/SearchBar.jsx
+++ Frontend/src/components/SearchBar.jsx
@@ -3,11 +3,12 @@ import { Avatar } from "antd";
 import React from "react";
 import { CiCirclePlus } from "react-icons/ci";
 import useAuth from "../hooks/useAuth";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { setGroupChatModal } from "../store/asyncCalls";
 
 function SearchBar() {
   const { user } = useAuth();
-  const { setGroupChatModal } = useSelector((state) => state.users);
+  const dispatch = useDispatch();
 
   return (
     <div>
@@ -22,8 +23,8 @@ function SearchBar() {
         className="w-full p-1 mt-3 rounded-md text-white placeholder:text-white border  focus:border-amber-600"
       />
       <p
-        className="px-5 mt-3 text-white py-1 rounded-4xl bg-amber-600 w-fit cursor-pointer"
-        onClick={setGroupChatModal}
+        className="px-5 mt-3 text-white py-1 rounded-4xl bg-amber-600 w-fit cursor-pointer active:scale-75 transition-all"
+        onClick={() => dispatch(setGroupChatModal())}
       >
         <CiCirclePlus className="inline-block" size={"25px"} /> Group
       </p>
