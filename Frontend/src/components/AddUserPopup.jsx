2b16423c00cfa2520a1eb0e4a6c76f7db909209a
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2025-04-18 21:15:58 +0530

    New : user adding for group chat created

---
+++ Frontend/src/components/AddUserPopup.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function AddUserPopup() {
+  return (
+    <div>
+      <h1>Add user popups</h1>
+    </div>
+  );
+}
+
+export default AddUserPopup;
