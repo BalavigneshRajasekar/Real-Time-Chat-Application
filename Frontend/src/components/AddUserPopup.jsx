78912bae7a9034dcd003909cb509801f92d1cd47
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2025-04-21 22:13:45 +0530

    New :  prod Url setuped and close popup function added for user selection in group creation modal

--- Frontend/src/components/AddUserPopup.jsx
+++ Frontend/src/components/AddUserPopup.jsx
@@ -2,7 +2,8 @@
 import { Avatar } from "antd";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setAddGroupUsers } from "../store/asyncCalls";
+import { MdCancel } from "react-icons/md";
+import { setAddGroupUsers, setAddUserPopup } from "../store/asyncCalls";
 
 function AddUserPopup() {
   const dispatch = useDispatch();
@@ -14,8 +15,14 @@ function AddUserPopup() {
   };
   return (
     <div className="w-fit h-fit bg-white rounded-2xl absolute top-72 p-3">
-      <h1>Select User</h1>
-      <div className="min-h-52 ">
+      <div className="flex justify-between  items-center ">
+        <h1>Select user</h1>
+        <MdCancel
+          className="inline-block text-2xl hover:text-red-700 active:scale-75 transition-all"
+          onClick={() => dispatch(setAddUserPopup())}
+        />
+      </div>
+      <div className="min-h-fit max-h-52 overflow-y-auto mt-3">
         {receiverData.map((users, i) => (
           <div key={i} className="flex gap-3">
             <input type="checkbox" onChange={(e) => addUsers(e, users)}></input>
@@ -23,10 +30,6 @@ function AddUserPopup() {
             <h2>{users.username}</h2>
           </div>
         ))}
-      </div>
-      <div className="flex justify-around">
-        <button>close</button>
-        <button>Add</button>
       </div>
     </div>
   );
